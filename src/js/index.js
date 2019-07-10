513c9482c703553740a3b89380e9af4fcb9727d0
Roma Yeremenko <warkoman1@gmail.com>
2019-07-10 23:47:31 +0300

    javascript tasks 2

--- src/js/index.js
+++ src/js/index.js
@@ -27,7 +27,44 @@ const o = {
 };
 
 
-document.addEventListener("DOMContentLoaded", ready);
+
+var name, members;
+
+function GetInfoAboutMessage() {
+    var arrDate = [];
+
+    function check(date) {
+        if (arrDate.indexOf(date) == -1) {
+            arrDate.push(date);
+
+            var dateBox = document.createElement("div");
+            dateBox.className = "dateMessage alignCenter sairaLight14";
+            var dateMessage = document.createElement("p");
+            var text = document.createTextNode(date);
+            dateMessage.appendChild(text);
+            dateBox.appendChild(dateMessage);
+            var roote = document.getElementById("parentMessages");
+            roote.appendChild(dateBox);
+        }
+
+    }
+
+    this.getTime = function (message) {
+        return message.datetime.substr(11, 5);
+    };
+
+    this.getDate = function (message) {
+        var date = message.datetime.substr(0, 10);
+        check(date);
+    };
+
+    this.getName = function (message) {
+        for (var i = 0; i < members.length; i++) {
+            if (members[i]["user_id"] == message["user_id"]) return members[i].username;
+        }
+    };
+
+}
 
 function ready() {
     var xhr = new XMLHttpRequest();
@@ -40,7 +77,7 @@ function ready() {
             alert(xhr.status + ': ' + xhr.statusText);
         } else {
             try {
-                var members = JSON.parse(xhr.responseText);
+                members = JSON.parse(xhr.responseText);
             } catch (e) {
                 alert("Некорректный ответ " + e.message);
             }
@@ -94,7 +131,7 @@ document.getElementById('buttonEnter').addEventListener('click', function () {
     check.innerHTML = "";
     document.getElementById("uname").style.border = 'inherit';
 
-    var name = document.getElementById("uname").value;
+    name = document.getElementById("uname").value;
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://studentschat.herokuapp.com/users/', true);
 
@@ -121,6 +158,8 @@ function getName(name, chat) {
     chat.forEach(function(chatInfo) {
         if (chatInfo.username == name) {
             nameuser.innerHTML = name;
+            ready();
+            loadMessages();
             document.querySelector('.bg-modal').style.display = 'none';
             return 0;
         }
@@ -134,7 +173,7 @@ function getName(name, chat) {
 }
 
 document.getElementById('buttonReg').addEventListener('click', function () {
-    var name = document.getElementById("uname").value;
+    name = document.getElementById("uname").value;
     var xhr = new XMLHttpRequest();
 
     xhr.open('POST', 'https://studentschat.herokuapp.com/users/register', true);
@@ -150,5 +189,87 @@ document.getElementById('buttonReg').addEventListener('click', function () {
 
     xhr.send(JSON.stringify({username : name}));
 
-    document.querySelector('.bg-modal').style.display = 'none';
+    setTimeout( function(){
+        nameuser.innerHTML = name;
+        document.querySelector('.bg-modal').style.display = 'none';
+        ready();loadMessages();}, 1000);
 });
+
+function getMessages(messages, infoAboutMessage) {
+
+    messages.forEach(function (message) {
+        showMessage(message);
+    });
+
+    function showMessage(message) {
+        infoAboutMessage.getDate(message);
+        var roote = document.getElementById("parentMessages");
+        var element = document.createElement("div");
+        element.className = "chatMessage alignMessage";
+
+        var avatar = document.createElement("div");
+        avatar.classList.add("chatAvatar");
+        element.appendChild(avatar);
+
+        var boxForMessage = document.createElement("div");
+        boxForMessage.classList.add("boxForMessage");
+
+
+        var messageText = document.createElement("div");
+        messageText.classList.add("messageText", "sairaLight14");
+        console.log(name);
+        if(infoAboutMessage.getName(message) == name) {
+            boxForMessage.classList.add("boxForMessageSelf");
+            messageText.classList.add("messageTextSelf");
+        }
+
+        var nameMember = document.createElement("div");
+        nameMember.classList.add("name", "sairaRegular18");
+        var nameDeliver = document.createTextNode(infoAboutMessage.getName(message));
+        nameMember.appendChild(nameDeliver);
+
+
+        var textBlock = document.createElement("p");
+        var text = document.createTextNode(message.message);
+        textBlock.appendChild(text);
+
+        messageText.appendChild(nameMember);
+        messageText.appendChild(textBlock);
+
+        var timeMessage = document.createElement("div");
+        timeMessage.classList.add("timeMessage", "sairaLight14");
+        var time = document.createTextNode(infoAboutMessage.getTime(message));
+        timeMessage.appendChild(time);
+
+        boxForMessage.appendChild(messageText);
+        boxForMessage.appendChild(timeMessage);
+
+        element.appendChild(avatar);
+        element.appendChild(boxForMessage);
+        roote.appendChild(element);
+    }
+
+
+}
+
+function loadMessages() {
+    var xhrMessages = new XMLHttpRequest();
+    xhrMessages.open('GET', 'https://studentschat.herokuapp.com/messages/', true);
+    xhrMessages.onreadystatechange = function () {
+
+        if (xhrMessages.readyState != 4) return;
+        if (xhrMessages.status != 200) {
+            alert(xhrMessages.status + ': ' + xhrMessages.statusText);
+        } else {
+            try {
+                var messages = JSON.parse(xhrMessages.responseText);
+            } catch (e) {
+                alert("Некорректный ответ " + e.message);
+            }
+            var infoAboutMessage = new GetInfoAboutMessage();
+            getMessages(messages, infoAboutMessage);
+        }
+    }
+
+    xhrMessages.send();
+}
